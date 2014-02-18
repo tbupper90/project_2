696a2918b1257a2379cfe5f19d306fed0d2569e8
avkrakkle <thayer.terry.a@gmail.com>
2014-02-18 17:40:25 -0600

    JUnit tests
    Here are all the nonsense JUnit tests I came up with. With the exception
    of the last method, sortTest is actually a correct usage (I think). The
    others do lame things like "test" constructors. DriverTest is the worst
    offender, where I clearly only just learned about "reflection." At least
    there's no warnings.

---
+++ src/PromptTest.java
@@ -0,0 +1,44 @@
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+
+public class PromptTest {
+
+    @Test
+    public void testGetFiles() {
+        // Make sure user input works
+        Prompt.getFiles();
+    }
+
+    @Test
+    public void testGetDataType() {
+        // No asserts; dependent on user input
+        ArrayList<?> list = Prompt.getDataType();
+        System.out.println(list.getClass());
+    }
+
+    @Test
+    public void testGetSortMethod() {
+        // No asserts; dependent on user input
+        // Tests for actual sorting methods are in SortTest.java
+        ArrayList<?> list = Prompt.getSortMethod();
+        System.out.println(list.getClass());
+    }
+
+    @Test
+    public void testGetOutputPreference() {
+        // Make sure user input works
+        Prompt.getOutputPreference();
+    }
+
+    @Test
+    public void testGetContinue() {
+        // Make sure user input works and reflects intent; display the result
+        boolean testExit = Prompt.getContinue();
+        System.out.println(testExit);
+    }
+
+}
---
+++ src/sortTest.java
@@ -0,0 +1,73 @@
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+
+public class sortTest {
+    
+    public ArrayList<City> makeTestList() {
+        // List with unique out-of-order elements to test sorting methods
+        ArrayList<City> testList = new ArrayList<City>();
+        testList.add(new City("Name1", "333", "22", "Coun1", "N3", "E2", "1"));
+        testList.add(new City("Name2", "111", "33", "Coun2", "N1", "E3", "2"));
+        testList.add(new City("Name3", "222", "11", "Coun3", "N2", "E1", "3"));
+        return testList;
+    }
+    @Test
+    public void testSortByArea() {
+        ArrayList<City> list = makeTestList();
+        sort.sortByArea(list);
+        assertEquals(list.get(0).getArea(), "11");
+    }
+
+    @Test
+    public void testSortByPopulation() {
+        ArrayList<City> list = makeTestList();
+        sort.sortByPopulation(list);
+        assertEquals(list.get(0).getPop(), "111");
+    }
+
+    @Test
+    public void testSortByLat() {
+        ArrayList<City> list = makeTestList();
+        sort.sortByLat(list);
+        assertEquals(list.get(0).getLat(), "N1");
+    }
+
+    @Test
+    public void testSortByLon() {
+        ArrayList<City> list = makeTestList();
+        sort.sortByLon(list);
+        assertEquals(list.get(0).getLon(), "E1");
+    }
+
+    @Test
+    public void testSortByElev() {
+        ArrayList<City> list = makeTestList();
+        sort.sortByArea(list);
+        assertEquals(list.get(0).getElev(), "1");
+    }
+
+    @Test
+    public void testSortLexi() {
+        ArrayList<City> list = makeTestList();
+        sort.sortLexi(list);
+        assertEquals(list.get(0).getName(), "Name1");
+    }
+
+    @Test
+    public void testSortRandomly() {
+        // Nothing to assert for a random sort, so run and print the results to
+        // ensure the method works
+        ArrayList<City> list = makeTestList();
+        for (int i = 0; i < 3; i++) {
+            sort.sortRandomly(list);
+            for (City iter: list) {
+                System.out.println(iter.getName());
+            }            
+        }
+    }
+
+}
