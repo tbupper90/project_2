b8524fb58d42df0cca99dbea0bc63900a549c0b3
avkrakkle <thayer.terry.a@gmail.com>
2014-02-26 21:06:13 -0600

    JUnits done
    DriverTest deleted. PromptTest simplified.

--- src/PromptTest.java
+++ src/PromptTest.java
@@ -14,7 +14,7 @@ public class PromptTest {
         return testList;
     }
 
-    /*
+    
     @Test
     public void testMultiple() {
         // Make sure user input works
@@ -34,50 +34,4 @@ public class PromptTest {
         System.out.println(testExit);
         
     }
-    */
-    
-    @Test
-    public void testGetFiles() {
-        // Make sure user input works
-        Prompt.getFiles();
-    }
-    
-
-    
-    @Test
-    public void testGetDataType() {
-        // No asserts; dependent on user input
-        System.out.println(Prompt.getDataType());
-    }
-    
-    
-    
-    @Test
-    public void testGetSortMethod() {
-        // No asserts; dependent on user input
-        // Tests for actual sorting methods are in SortTest.java
-        String data = Prompt.getDataType();
-        System.out.println(Prompt.getSortMethod(data));
-    }
-    
-    
-    /*
-    @Test
-    public void testGetOutputPreference() {
-        ArrayList<? extends Region> list = new ArrayList<City>();
-        // Make sure user input works
-        Prompt.getOutputPreference();
-    }
-    */
-
-    
-    @Test
-    public void testGetContinue() {
-        // Make sure user input works and reflects intent; display the result
-        boolean testExit = Prompt.getContinue();
-        System.out.println(testExit);
-    }
-    
-    
-
 }
