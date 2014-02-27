511dbd99ea1ac9aa195c3f51b33478290340eff3
avkrakkle <thayer.terry.a@gmail.com>
2014-02-26 21:01:55 -0600

    sortTest revision
    Revising JUnits. sortTest done.

--- src/PromptTest.java
+++ src/PromptTest.java
@@ -1,5 +1,3 @@
-import static org.junit.Assert.*;
-
 import java.util.ArrayList;
 
 import org.junit.Test;
@@ -7,7 +5,16 @@ import org.junit.Test;
 
 public class PromptTest {
 
-    
+    public ArrayList<City> makeTestList() {
+        // List with unique out-of-order elements to test sorting methods
+        ArrayList<City> testList = new ArrayList<City>();
+        testList.add(new City("Name1", "333", "22", "Coun1", "N3", "E2", "1"));
+        testList.add(new City("Name2", "111", "33", "Coun2", "N1", "E3", "2"));
+        testList.add(new City("Name3", "222", "11", "Coun3", "N2", "E1", "3"));
+        return testList;
+    }
+
+    /*
     @Test
     public void testMultiple() {
         // Make sure user input works
@@ -27,24 +34,24 @@ public class PromptTest {
         System.out.println(testExit);
         
     }
-
-    /*
+    */
+    
     @Test
     public void testGetFiles() {
         // Make sure user input works
         Prompt.getFiles();
     }
-    */
+    
 
-    /*
+    
     @Test
     public void testGetDataType() {
         // No asserts; dependent on user input
         System.out.println(Prompt.getDataType());
     }
-    */
     
-    /*
+    
+    
     @Test
     public void testGetSortMethod() {
         // No asserts; dependent on user input
@@ -52,24 +59,25 @@ public class PromptTest {
         String data = Prompt.getDataType();
         System.out.println(Prompt.getSortMethod(data));
     }
-    */
+    
     
     /*
     @Test
     public void testGetOutputPreference() {
+        ArrayList<? extends Region> list = new ArrayList<City>();
         // Make sure user input works
         Prompt.getOutputPreference();
     }
     */
 
-    /*
+    
     @Test
     public void testGetContinue() {
         // Make sure user input works and reflects intent; display the result
         boolean testExit = Prompt.getContinue();
         System.out.println(testExit);
     }
-    */
+    
     
 
 }
--- src/sortTest.java
+++ src/sortTest.java
@@ -7,7 +7,16 @@ import org.junit.Test;
 
 public class sortTest {
     
-    public ArrayList<City> makeTestList() {
+    public ArrayList<Region> makeTestList() {
+        // List with unique out-of-order elements to test sorting methods
+        ArrayList<Region> testList = new ArrayList<Region>();
+        testList.add(new Continent("Name1", "333", "22"));
+        testList.add(new Continent("Name2", "111", "33"));
+        testList.add(new Continent("Name3", "222", "11"));
+        return testList;
+    }
+
+    public ArrayList<City> makeTestCity() {
         // List with unique out-of-order elements to test sorting methods
         ArrayList<City> testList = new ArrayList<City>();
         testList.add(new City("Name1", "333", "22", "Coun1", "N3", "E2", "1"));
@@ -16,46 +25,46 @@ public class sortTest {
         return testList;
     }
     
-    /*
+    
     @Test
     public void testSortByArea() {
-        ArrayList<City> list = makeTestList();
+        ArrayList<Region> list = makeTestList();
         sort.sortByArea(list);
-        assertEquals(list.get(0).getArea(), "11");
+        assertEquals(list.get(0).getArea(), "33");
     }
 
     @Test
     public void testSortByPopulation() {
-        ArrayList<City> list = makeTestList();
+        ArrayList<Region> list = makeTestList();
         sort.sortByPopulation(list);
-        assertEquals(list.get(0).getPop(), "111");
+        assertEquals(list.get(0).getPop(), "333");
     }
 
     @Test
     public void testSortByLat() {
-        ArrayList<City> list = makeTestList();
+        ArrayList<City> list = makeTestCity();
         sort.sortByLat(list);
         assertEquals(list.get(0).getLat(), "N1");
     }
 
     @Test
     public void testSortByLon() {
-        ArrayList<City> list = makeTestList();
+        ArrayList<City> list = makeTestCity();
         sort.sortByLon(list);
         assertEquals(list.get(0).getLon(), "E1");
     }
 
     @Test
     public void testSortByElev() {
-        ArrayList<City> list = makeTestList();
-        sort.sortByArea(list);
-        assertEquals(list.get(0).getElev(), "1");
+        ArrayList<City> list = makeTestCity();
+        sort.sortByElev(list);
+        assertEquals(list.get(0).getElev(), "3");
     }
 
     @Test
     public void testSortLexi() {
-        ArrayList<City> list = makeTestList();
-        sort.sortLexi(list);
+        ArrayList<Region> list = makeTestList();
+        sort.sortByLexi(list);
         assertEquals(list.get(0).getName(), "Name1");
     }
 
@@ -63,15 +72,15 @@ public class sortTest {
     public void testSortRandomly() {
         // Nothing to assert for a random sort, so run and print the results to
         // ensure the method works
-        ArrayList<City> list = makeTestList();
+        ArrayList<Region> list = makeTestList();
         for (int i = 0; i < 3; i++) {
             sort.sortRandomly(list);
-            for (City iter: list) {
+            for (Region iter: list) {
                 System.out.println(iter.getName());
             }            
         }
     }
     
-    */
+    
 
 }
